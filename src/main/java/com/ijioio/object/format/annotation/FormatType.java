080c9ab643cf5a818af39c45379f24a5b45466ac
abeleshev <abeleshev@gmail.com>
2022-01-02 22:13:40 +0900

    Add annotations

---
+++ src/main/java/com/ijioio/object/format/annotation/FormatType.java
@@ -0,0 +1,13 @@
+package com.ijioio.object.format.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface FormatType {
+
+	public String value() default "";
+}
