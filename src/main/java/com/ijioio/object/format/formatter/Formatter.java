1322b22c0dfea4c7d6053b1a96c305f0c79705f3
abeleshev <abeleshev@gmail.com>
2022-01-23 12:59:20 +0300

    Add extractor and formatter interfaces

--- src/main/java/com/ijioio/object/format/formatter/Formatter.java
+++ src/main/java/com/ijioio/object/format/formatter/Formatter.java
@@ -7,14 +7,14 @@ import com.ijioio.object.format.object.ObjectHolder;
 
 public interface Formatter<V> {
 
-	public String format(ObjectHolder<V> object, Configuration configuration, String separator, String pattern,
+	public String format(ObjectHolder<V> objectHolder, Configuration configuration, String separator, String pattern,
 			Locale locale) throws Exception;
 
-	public static final class Default implements Formatter<Void> {
+	public static final class Empty implements Formatter<Void> {
 
 		@Override
-		public String format(ObjectHolder<Void> object, Configuration configuration, String separator, String pattern,
-				Locale locale) throws Exception {
+		public String format(ObjectHolder<Void> objectHolder, Configuration configuration, String separator,
+				String pattern, Locale locale) throws Exception {
 			throw new Exception();
 		}
 	}
