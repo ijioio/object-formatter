c13718dc2fc73d977ff9560e3ff8b4b50d1f4053
abeleshev <abeleshev@gmail.com>
2022-07-02 13:17:48 +0900

    Add builder exception (#19)

---
+++ src/test/java/com/ijioio/object/format/test/builder/BuilderException.java
@@ -0,0 +1,10 @@
+package com.ijioio.object.format.test.builder;
+
+public class BuilderException extends RuntimeException {
+
+	private static final long serialVersionUID = -3098496728792779983L;
+
+	public BuilderException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
