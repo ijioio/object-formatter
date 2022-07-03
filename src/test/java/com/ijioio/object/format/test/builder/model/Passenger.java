4ec1328024d484724690e240951da123cc427174
abeleshev <abeleshev@gmail.com>
2022-07-03 13:31:29 +0900

    Add test models (#30)

---
+++ src/test/java/com/ijioio/object/format/test/builder/model/Passenger.java
@@ -0,0 +1,7 @@
+package com.ijioio.object.format.test.builder.model;
+
+import com.ijioio.object.format.test.builder.PassengerBuilder.PassengerPrototypeAdapter;
+
+public class Passenger extends PassengerPrototypeAdapter {
+	// Empty
+}
---
+++ src/test/java/com/ijioio/object/format/test/builder/model/Segment.java
@@ -0,0 +1,7 @@
+package com.ijioio.object.format.test.builder.model;
+
+import com.ijioio.object.format.test.builder.SegmentBuilder.SegmentPrototypeAdapter;
+
+public class Segment extends SegmentPrototypeAdapter {
+	// Empty
+}
